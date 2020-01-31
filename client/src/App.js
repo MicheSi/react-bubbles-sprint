d0d24437bb4f7f212c8af994f0995dda8fd62b34
mlazzell17@gmail.com <MicheSi>
2020-01-31 09:53:28 -0700

    created log in form, routes to bubbles page upon login

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // components
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 // styles
 import "./styles.scss";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <PrivateRoute path='/bubbles' component={BubblePage}/>
+          <Route exact path="/" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
